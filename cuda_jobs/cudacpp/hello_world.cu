579ca29c601741ca7fad805d624b55a00548323d
UTSACollab <86381408+UTSACollab@users.noreply.github.com>
2021-07-13 08:46:35 -0500

    Create hello_world CPP+cuda code file

---
+++ cuda_jobs/cudacpp/hello_world.cu
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <cuda.h>
+#include <stdlib.h>
+#include <unistd.h>
+using namespace std;
+
+int main(){
+
+ int count, err;
+ cudaGetDeviceCount(&count);
+ cout<<"device count = "<<count<<"\n";
+ if(err = cudaSetDevice(count-1)){
+    cout<<"cudaSetDevice error, "<<err<<"\n";
+ }
+ return 0;
+}
