99a2746628385634780aa9357c3788e2d8a4cc97
UTSACollab <86381408+UTSACollab@users.noreply.github.com>
2021-07-13 11:49:31 -0500

    C+multiexes+hello_world code

---
+++ multiExes/serial/hello_world.c
@@ -0,0 +1,10 @@
+# include <stdio.h>
+# include<stdlib.h>
+int main(int argc, char *argv[]){
+    int n = atoi( argv[1]);
+    for(int i=1;i<=n;i++)
+    {
+     printf("Hello World!!: %d \n",i);
+    }
+        return 0;
+}
