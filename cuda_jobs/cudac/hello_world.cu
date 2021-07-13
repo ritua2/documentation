7827d41977caaa2288bf5c96dfeab4063aedd02c
UTSACollab <86381408+UTSACollab@users.noreply.github.com>
2021-07-13 08:44:04 -0500

    Create hello_world.cu C+cuda code

---
+++ cuda_jobs/cudac/hello_world.cu
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <cuda.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+int main(){
+
+ int count, err;
+ cudaGetDeviceCount(&count);
+ printf("device count = %d\n", count);
+ if(err = cudaSetDevice(count-1)){
+    printf("cudaSetDevice error, %d\n", err);
+ }
+ return 0;
+}
