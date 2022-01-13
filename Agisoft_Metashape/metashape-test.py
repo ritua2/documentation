6b295148f46f107cf57e5e80f4d7f5e4044b2993
git4rzutsa <89165464+git4rzutsa@users.noreply.github.com>
2022-01-13 16:35:33 -0600

    Create metashape-test.py

---
+++ Agisoft_Metashape/metashape-test.py
@@ -0,0 +1,19 @@
+import Metashape
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--file", help='Full Path to Project File (monument.psz)')
+args = vars(parser.parse_args())
+
+label = 'Chunk 1'
+
+doc = Metashape.app.document
+doc.open(path = args['file'])
+chunk = doc.chunk
+
+if label == doc.chunks[0].label:
+  print("Success: Able to confirm chunk keys from sample project.")
+else:
+  print("Error: Unable to confirm chunk keys from sample project.")
+
+doc.clear()
